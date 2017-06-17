10b4463f8db253ef350a5f6ecc9870746c4765fb
Philip White <philipwhite@cedarville.edu>
2017-06-17 09:25:22 -0400

    Power Standby to HIGH.
    Wheels can't drive if the standby is LOW.

--- WheelTest/WheelTest.ino
+++ WheelTest/WheelTest.ino
@@ -1,55 +1,53 @@
-// Pins
+// Wheels
 const int WHEEL_DIR_LB = 26;
 const int WHEEL_DIR_LF = 28;
 const int WHEEL_DIR_RB = 34;
 const int WHEEL_DIR_RF = 36;
 
-const int WHEEL_PWM_L = 7;
-const int WHEEL_PWM_R = 8;
+const int WHEEL_SPEED_L = 7;
+const int WHEEL_SPEED_R = 8;
 
-const int WHEEL_PWM_STBY = 33;
+const int WHEEL_STBY = 33;
+
 
 void leftDrive(int speed) {
+  // HIGH-LOW combination drives wheel forward.
   digitalWrite(WHEEL_DIR_LF, HIGH);
   digitalWrite(WHEEL_DIR_LB, LOW);
-  analogWrite(WHEEL_PWM_L, speed);
+  analogWrite(WHEEL_SPEED_L, speed);
 }
 
 void rightDrive(int speed) {
+  // HIGH-LOW combination drives wheel forward.
   digitalWrite(WHEEL_DIR_RF, HIGH);
   digitalWrite(WHEEL_DIR_RB, LOW);
-  analogWrite(WHEEL_PWM_R, speed);
+  analogWrite(WHEEL_SPEED_R, speed);
 }
 
 void setup() {
-  // Left wheel
-  pinMode(WHEEL_PWM_L, OUTPUT);
-  analogWrite(WHEEL_PWM_L, 0);
-
+  // Wheels
+  pinMode(WHEEL_SPEED_L, OUTPUT);
   pinMode(WHEEL_DIR_LF, OUTPUT);
   pinMode(WHEEL_DIR_LB, OUTPUT);
-
-  digitalWrite(WHEEL_DIR_LF, HIGH);
-  digitalWrite(WHEEL_DIR_LB, LOW);
-
-  // Right wheel
-  pinMode(WHEEL_PWM_R, OUTPUT);
-  analogWrite(WHEEL_PWM_R, 0);
-
+  
+  pinMode(WHEEL_SPEED_R, OUTPUT);
   pinMode(WHEEL_DIR_RF, OUTPUT);
   pinMode(WHEEL_DIR_RB, OUTPUT);
-
-  digitalWrite(WHEEL_DIR_RF, HIGH);
+  
+  pinMode(WHEEL_STBY, OUTPUT);
+  
+  digitalWrite(WHEEL_DIR_LF, LOW);
+  digitalWrite(WHEEL_DIR_LB, LOW);
+  
+  digitalWrite(WHEEL_DIR_RF, LOW);
   digitalWrite(WHEEL_DIR_RB, LOW);
-
-  // Standby
-  pinMode(WHEEL_PWM_STBY, OUTPUT);
-  digitalWrite(WHEEL_PWM_STBY, HIGH);
-
+  
+  digitalWrite(WHEEL_STBY, HIGH);
   Serial.begin(115200);
 }
 
 void loop() {
+  Serial.println("hello");
   leftDrive(100);
-  rightDrive(200);
+  rightDrive(100);
 }
